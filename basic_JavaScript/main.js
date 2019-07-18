04d16474749b31f9fd97f3cb4a33a71128e96f00
RealFilllykung <filllykunggm@gmail.com>
2019-07-19 00:54:44 +0700

    เพิ่มคำสั่ง basic ที่้เราใช้กับ  JavaScript

---
+++ basic_JavaScript/main.js
@@ -0,0 +1,30 @@
+//alert('Hello');
+//อันนีืคือ command ที่มันจะ popup alert มาให้ในหน้าเว็บเรา
+
+//ตรงนี้คือมันจะ print คำที่เราใส่ไว้เข้าไปใน console
+console.log('Hello');
+console.log('This loop has ended');
+console.log('This is the error');
+
+//วิธีประกาศตัวแปล
+let a;
+a = 30;
+console.log(a);
+
+//ตั้งค่าตัวแปลแบบ constant, ตัวแปลนี้ไม่สามารถแก้ค่าได้
+const b = 100;
+console.log(b);
+//b = 20;
+
+//String Numbers Boolean null undefined
+let name = "Fill";
+let myNumber = 20;
+let mySingleStatus = true;
+let myGF = null;
+let myUndefined = undefined;
+//undefined คือตัวแปลที่เอาไว้ไม่ให้มันถูกแก้ค่าได้
+
+//Print ว่าตัวแปลนี้เป็นตัวแปลชนิดอะไร
+console.log(typeof(name));
+
+
