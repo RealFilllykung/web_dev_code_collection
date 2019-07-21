45ec0a3ee00f1e1f28d91aab930863d6001a35e6
RealFilllykung <filllykunggm@gmail.com>
2019-07-21 15:12:49 +0700

    เพิ่มคำสั่ง function literals OOP

--- OOP_JavaScript/main.js
+++ OOP_JavaScript/main.js
@@ -1,19 +1,33 @@
+//Literals จะเป็นอารมณ์ structure
+var myLiterals ={
+    name: "Fill",
+    age: 20,
+    getInfo: function(){
+        console.log(this.name + " " + this.age);
+    }
+}
+myLiterals.getInfo();
+
 //Basic ของ OOP นั้นส่วนมากจะมองภาพ variable ของเราเป็น class
 class People{
+    //constructor ของ class People
     constructor(fname,lname,age){
         this.fname = fname;
         this.lname = lname;
         this.age = age;
     }
 
+    //Function ของ class People
     greeting = function(){
         console.log("A person name " + this.fname + " " + this.lname + " with an age of " + this.age + " say hi to you");
     }
 }
 
+//วิธีสร้าง object ใหม่
 var p1 = new People("Thartahep","Klinka-or",20);
 p1.greeting();
 
+//Inheritance ของ class People
 class student extends People{
     constructor(fname,lname,age,gpa){
         super(fname,lname,age);
